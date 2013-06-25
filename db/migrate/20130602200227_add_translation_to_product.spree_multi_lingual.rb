8d9de90b0a02a55f7997ac4fc0f58a46f951f126
picazoH <hector@ahaaa.es>
2013-06-25 22:03:59 +0200

    migration fixed and spree_flexi_variants.git bumped

--- db/migrate/20130602200227_add_translation_to_product.spree_multi_lingual.rb
+++ db/migrate/20130602200227_add_translation_to_product.spree_multi_lingual.rb
@@ -11,9 +11,9 @@
 class AddTranslationToProduct < ActiveRecord::Migration
   def up
 
-    Spree::Config[:track_inventory_levels] = false
+    #Spree::Config[:track_inventory_levels] = false
     Spree::Product.create_translation_table! :name => :string, :description => :text, :meta_description => :string, :meta_keywords => :string
-    Spree::Config[:track_inventory_levels] = true
+    #Spree::Config[:track_inventory_levels] = true
   end
 
   def down
